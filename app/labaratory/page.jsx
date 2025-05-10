02b8c3c01187c36d9fc11c49c18116038c86affb
karimoffabdulatif <akarimoff001@gmail.com>
2025-05-10 17:47:00 +0500

    .

--- app/labaratory/page.jsx
+++ app/labaratory/page.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+'use client';
+import React, { useState, useMemo } from 'react';
 import Link from 'next/link';
 
 const labData = [
@@ -14,6 +15,18 @@ const labData = [
     { title: "Potansiyometr - EMFni taqqoslash", image: "/potans.jpg", slug: "emf-taqqoslash" },
     { title: "Diyot xususiyatlari", image: "/dyd.png", slug: "/diyxus" },
     { title: "Tranzistor xususiyatlari", image: "/transistor.jpg", slug: "/trxus" },
+    { title: "Qavariq oyna - Fokus uzunligi", image: "/qavariq.jpg", slug: "/trxus" },
+    { title: "Konkav linzalari - Fokus uzunligi", image: "/fokus.jpg", slug: "/trxus" },
+    { title: "Suyuqlikning sinishi indeksi", image: "/water.jpg", slug: "/trxus" },
+    { title: "Galvanometrni voltmetrga aylantirish", image: "/gl.jpg", slug: "/trxus" },
+    { title: "Sayohat mikroskopidan foydalangan holda shisha plitaning sinishi indeksi", image: "/shisha.jpg", slug: "/trxus" },
+    { title: "Voltaj o'lchash moslamasi cheksiz qarshilikka ega", image: "/volt.jpg", slug: "/trxus" },
+    { title: "Induktiv sxemada induktsiyalangan emf", image: "/n.jpg", slug: "/trxus" },
+    { title: "Zaryadlar qaytaradi va zaryadlardan farqli ravishda tortadi", image: "/z.png", slug: "/trxus" },
+    { title: "Qarama-qarshi/bir xil yo'nalishda tok o'tkazuvchi ikki o'tkazgich o'rtasidagi itarish / tortishish", image: "/qarama.jpg", slug: "/trxus" },
+    { title: "Natriy xlorid qo'shilishining distillangan suvning elektr o'tkazuvchanligiga ta'siri", image: "/natriy.jpg", slug: "/trxus" },
+    { title: "Bobinda induktsiyalangan emf ishlab chiqarish", image: "/bunda.jpg", slug: "/trxus" },
+    { title: "Elektrostatik himoyani ko'rsatish", image: "/elektro.jpg", slug: "/trxus" },
     { title: "Ohm qonuni va qarshilik", image: "/om.jpg", slug: "/ohm" },
     { title: "Zener diyot", image: "/zener.JPG", slug: "/zener" },
     { title: "Galvanometrni ampermetrga aylantirish", image: "/galvan.jpg", slug: "/galvan" },
@@ -26,57 +39,45 @@ const labData = [
     { title: "Potansiyometr - EMFni taqqoslash", image: "/potans.jpg", slug: "emf-taqqoslash" },
     { title: "Diyot xususiyatlari", image: "/dyd.png", slug: "/diyxus" },
     { title: "Tranzistor xususiyatlari", image: "/transistor.jpg", slug: "/trxus" },
-    { title: "Ohm qonuni va qarshilik", image: "/om.jpg", slug: "/ohm" },
-    { title: "Zener diyot", image: "/zener.JPG", slug: "/zener" },
-    { title: "Galvanometrni ampermetrga aylantirish", image: "/galvan.jpg", slug: "/galvan" },
-    { title: "Metr ko'prigi - simning qarshiligi", image: "/metr.jpg", slug: "/metr" },
-    { title: "Potensiometr - hujayraning ichki qarshiligi", image: "/potensiometr.jpg", slug: "potensiometr-ichki-qarshilik" },
-    { title: "AC sonometri", image: "/ac.png", slug: "/ac" },
-    { title: "Diyotning xususiyatlari", image: "/diyod.jpg", slug: "/diyot" },
-    { title: "Transistorlar xususiyatlari", image: "/tran.jpg", slug: "/tran" },
-    { title: "Konkav oyna - UV usuli", image: "/koknav.jpg", slug: "/koknav" },
-    { title: "Potansiyometr - EMFni taqqoslash", image: "/potans.jpg", slug: "emf-taqqoslash" },
-    { title: "Diyot xususiyatlari", image: "/dyd.png", slug: "/diyxus" },
-    { title: "Tranzistor xususiyatlari", image: "/transistor.jpg", slug: "/trxus" },
-    { title: "Ohm qonuni va qarshilik", image: "/om.jpg", slug: "/ohm" },
-    { title: "Zener diyot", image: "/zener.JPG", slug: "/zener" },
-    { title: "Galvanometrni ampermetrga aylantirish", image: "/galvan.jpg", slug: "/galvan" },
-    { title: "Metr ko'prigi - simning qarshiligi", image: "/metr.jpg", slug: "/metr" },
-    { title: "Potensiometr - hujayraning ichki qarshiligi", image: "/potensiometr.jpg", slug: "potensiometr-ichki-qarshilik" },
-    { title: "AC sonometri", image: "/ac.png", slug: "/ac" },
-    { title: "Diyotning xususiyatlari", image: "/diyod.jpg", slug: "/diyot" },
-    { title: "Transistorlar xususiyatlari", image: "/tran.jpg", slug: "/tran" },
-    { title: "Konkav oyna - UV usuli", image: "/koknav.jpg", slug: "/koknav" },
-    { title: "Potansiyometr - EMFni taqqoslash", image: "/potans.jpg", slug: "emf-taqqoslash" },
-    { title: "Diyot xususiyatlari", image: "/dyd.png", slug: "/diyxus" },
-    { title: "Tranzistor xususiyatlari", image: "/transistor.jpg", slug: "/trxus" },
-    { title: "Ohm qonuni va qarshilik", image: "/om.jpg", slug: "/ohm" },
-    { title: "Zener diyot", image: "/zener.JPG", slug: "/zener" },
-    { title: "Galvanometrni ampermetrga aylantirish", image: "/galvan.jpg", slug: "/galvan" },
-    { title: "Metr ko'prigi - simning qarshiligi", image: "/metr.jpg", slug: "/metr" },
-    { title: "Potensiometr - hujayraning ichki qarshiligi", image: "/potensiometr.jpg", slug: "potensiometr-ichki-qarshilik" },
-    { title: "AC sonometri", image: "/ac.png", slug: "/ac" },
-    { title: "Diyotning xususiyatlari", image: "/diyod.jpg", slug: "/diyot" },
-    { title: "Transistorlar xususiyatlari", image: "/tran.jpg", slug: "/tran" },
-    { title: "Konkav oyna - UV usuli", image: "/koknav.jpg", slug: "/koknav" },
-    { title: "Potansiyometr - EMFni taqqoslash", image: "/potans.jpg", slug: "emf-taqqoslash" },
-    { title: "Diyot xususiyatlari", image: "/dyd.png", slug: "/diyxus" },
-    { title: "Tranzistor xususiyatlari", image: "/transistor.jpg", slug: "/trxus" },
-    { title: "Diyotning xususiyatlari", image: "/diyod.jpg", slug: "/diyot" },
-    { title: "Transistorlar xususiyatlari", image: "/tran.jpg", slug: "/tran" },
-    { title: "Konkav oyna - UV usuli", image: "/koknav.jpg", slug: "/koknav" },
-    { title: "Potansiyometr - EMFni taqqoslash", image: "/potans.jpg", slug: "emf-taqqoslash" },
-    { title: "Diyot xususiyatlari", image: "/dyd.png", slug: "/diyxus" },
-    { title: "Tranzistor xususiyatlari", image: "/transistor.jpg", slug: "/trxus" },
-   
+    { title: "Qavariq oyna - Fokus uzunligi", image: "/qavariq.jpg", slug: "/trxus" },
+    { title: "Konkav linzalari - Fokus uzunligi", image: "/fokus.jpg", slug: "/trxus" },
+    { title: "Suyuqlikning sinishi indeksi", image: "/water.jpg", slug: "/trxus" },
+    { title: "Galvanometrni voltmetrga aylantirish", image: "/gl.jpg", slug: "/trxus" },
+    { title: "Sayohat mikroskopidan foydalangan holda shisha plitaning sinishi indeksi", image: "/shisha.jpg", slug: "/trxus" },
+    { title: "Voltaj o'lchash moslamasi cheksiz qarshilikka ega", image: "/volt.jpg", slug: "/trxus" },
+    { title: "Induktiv sxemada induktsiyalangan emf", image: "/n.jpg", slug: "/trxus" },
+    { title: "Zaryadlar qaytaradi va zaryadlardan farqli ravishda tortadi", image: "/z.png", slug: "/trxus" },
+    { title: "Qarama-qarshi/bir xil yo'nalishda tok o'tkazuvchi ikki o'tkazgich o'rtasidagi itarish / tortishish", image: "/qarama.jpg", slug: "/trxus" },
+    { title: "Natriy xlorid qo'shilishining distillangan suvning elektr o'tkazuvchanligiga ta'siri", image: "/natriy.jpg", slug: "/trxus" },
+    { title: "Bobinda induktsiyalangan emf ishlab chiqarish", image: "/bunda.jpg", slug: "/trxus" },
+    { title: "Elektrostatik himoyani ko'rsatish", image: "/elektro.jpg", slug: "/trxus" },
 ];
 
 const Labaratory = () => {
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const filteredData = useMemo(() => {
+        return labData.filter((lab) =>
+            lab.title.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+    }, [searchQuery]);
+
     return (
-        <div className="pb-14 container  bg-gray-200">
+        <div className="pb-14 container bg-gray-200 min-h-screen">
             <h1 className="text-3xl font-bold text-center mb-6 pt-8">Fizika Laboratoriyasi</h1>
+
+            <div className="flex justify-center mb-6">
+                <input
+                    type="text"
+                    placeholder="Qidirish..."
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                    className="px-4 py-2 w-full max-w-md border border-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+                />
+            </div>
+
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-6 gap-6">
-                {labData.map((lab, index) => (
+                {filteredData.map((lab, index) => (
                     <Link key={index} href={`/labaratory/${lab.slug}`}>
                         <div className="bg-gradient-to-t from-blue-200 to-white p-4 rounded-lg shadow-lg hover:shadow-xl transition-shadow cursor-pointer">
                             <img src={lab.image} alt={lab.title} className="w-full h-32 object-cover rounded-t-lg" />
@@ -84,6 +85,9 @@ const Labaratory = () => {
                         </div>
                     </Link>
                 ))}
+                {filteredData.length === 0 && (
+                    <p className="text-center pt-[120px] text-2xl col-span-full text-gray-500">Hech narsa topilmadi.</p>
+                )}
             </div>
         </div>
     );
--- app/labaratory/zener/page.jsx
+++ app/labaratory/zener/page.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from 'react';
+import 'katex/dist/katex.min.css';
+import { BlockMath } from 'react-katex';
 
 const Zener = () => {
   return (
-    <div>Zener</div>
-  )
-}
+    <div className='container'>
+      <div className="bg-white text-gray-800">
+        <div className='justify-center mt-4 px-7 py-5 rounded-2xl bg-blue-100 items-center gap-5'>
+          <h1 className="text-3xl justify-center flex font-bold mb-4">Objective:</h1>
+          <p className="mb-4 text-[20px]">
+            A Zener diode is a heavily doped silicon crystal diode that allows current to flow in the forward direction just like a regular diode. Additionally, it allows current to flow in the reverse direction if the voltage is above a certain value known as the breakdown voltage. The breakdown voltage is also known as the Zener knee voltage.
 
-export default Zener
+            The device is named after American physicist Clarence Zener, who described the property related to the breakdown of electrical insulators.
+
+            The device consists of a reverse-biased, heavily doped PN junction diode that operates in the breakdown region. Conventional diodes and rectifiers never operate in the breakdown region, but the Zener diode can safely operate in this zone.
+          </p>
+        </div>
+
+        <div className='video flex bg-blue-100 rounded-2xl p-4 justify-center mt-6'>
+          <iframe
+            width="1500"
+            height="315"
+            src="https://www.youtube.com/embed/XhQqtdTlRus"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+            className='rounded-xl'
+          ></iframe>
+        </div>
+
+        <div className='bg-blue-100 mt-6 px-7 py-4 rounded-2xl mb-8'>
+          <h2 className="text-3xl flex justify-center font-bold mb-2">Theory:</h2>
+
+          <p className="mb-4 text-[18px]">
+            As the reverse voltage applied to a Zener diode increases, it reaches the breakdown voltage at which the Zener current rises significantly. In the breakdown region, further increase in reverse voltage does not increase the voltage across the Zener diode; it only increases the current. Thus, a constant voltage, called the Zener voltage (V<sub>z</sub>), is maintained across the Zener diode despite changes in supply voltage. Therefore, it acts as a voltage regulator.
+
+            The reverse characteristic is obtained by plotting reverse voltage along the X-axis and reverse current along the Y-axis. When the reverse voltage reaches a certain value, the reverse current increases rapidly, but the voltage across the diode remains constant. This constant voltage is known as the breakdown voltage, V<sub>z</sub>.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Zener;
